5b880b0b712e04c00cbd5709a4472d0219a25c08
Danny Fan <dannyfan@live.com>
2020-07-19 23:45:00 -0400

    feat: save songs selected in local storage

--- src/components/SongList/Song.js
+++ src/components/SongList/Song.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Song = (props) => {
     const imageSrc = require(`../../assets/images/${props.data.name}.png`);
     const isExclusive = props.data.kk_exclusive ? "is-Exclusive" : "";
+    const [ checked, setChecked ] = useState(props.checked);
+
+    const haveSong = (event) => {
+        setChecked(event.target.checked);
+        if (event.target.checked) {
+            console.log("add");
+            console.log(props.obtained);
+            props.setObtained((prevArray) => [...prevArray, props.number]);
+        } else {
+            console.log("remove");
+            console.log(props.obtained);
+            props.setObtained(
+                props.obtained.filter((number) => number !== props.number)
+            );
+        }
+    };
+
     return (
         <div className={`Song ${isExclusive}`}>
-            <input type="checkbox" id={props.number} name={props.number} value={props.number}/>
+            <input
+                type="checkbox"
+                checked={checked}
+                id={props.number}
+                name={props.number}
+                value={props.number}
+                onChange={haveSong}
+            />
             <label htmlFor={props.number}>
-                <img src={imageSrc} alt={props.data.name}/>
-                <div>{props.number}. {props.data.name}</div>
+                <img src={imageSrc} alt={props.data.name} />
+                <div>
+                    {props.number}. {props.data.name}
+                </div>
             </label>
         </div>
-    )
-}
+    );
+};
 
 export default Song;
--- src/components/SongList/SongList.js
+++ src/components/SongList/SongList.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Song from "./Song";
 import songData from "../../data.json";
 import "./Song.scss";
 
 const SongList = () => {
+    const initialSongs = localStorage.getItem("songs")
+        ? JSON.parse(localStorage.getItem("songs"))
+        : [];
+    const [obtained, setObtained] = useState(initialSongs);
+
+    useEffect(() => {
+        if (obtained >= 1) {
+            localStorage.setItem("songs", JSON.stringify(obtained));
+        }
+    });
+
     return (
         <div className="SongList">
             {Object.entries(songData).map((values, key) => {
                 const number = values[0];
                 const data = values[1];
-                return <Song key={key} data={data} number={number}/>
+                const checked = initialSongs.includes(number);
+                return (
+                    <Song
+                        key={key}
+                        checked={checked}
+                        data={data}
+                        number={number}
+                        obtained={obtained}
+                        setObtained={setObtained}
+                    />
+                );
             })}
+            <div className="SongList-number">{obtained.length}</div>
         </div>
-    )
-}
+    );
+};
 
 export default SongList;
